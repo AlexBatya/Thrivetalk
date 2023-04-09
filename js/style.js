5953b25d7b363650cc32440932eb0b4e6213ebab
AlexBatya <alex28.12.12@mail.ru>
2023-04-09 11:04:15 +1000

    'new'

---
+++ js/style.js
@@ -0,0 +1,12 @@
+$(document).ready(()=>{
+    const $header = $('header');
+    const $main = $('main');
+    const $footer = $('footer');
+
+    const $whyThriv = $('.whyThriv');
+    if($(window).width() <= 1024){
+        $('.img1').css({
+            "width": "80%",
+        })
+    }
+})
